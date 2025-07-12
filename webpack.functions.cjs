1a35e06788dda2e9628642db7707ac59b21824a5
prograk <akshay.malhotra17@gmail.com>
2025-07-12 09:55:05 +0530

    fix

--- webpack.functions.cjs
+++ webpack.functions.cjs
@@ -6,11 +6,25 @@ module.exports = {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /node_modules\/(?!@xenova)/, // only transpile xenova
+        // Only transpile @xenova and @huggingface modules (modern ESM)
+        include: [
+          path.resolve(__dirname, 'functions'),
+          path.resolve(__dirname, 'node_modules/@xenova'),
+          path.resolve(__dirname, 'node_modules/@huggingface'),
+        ],
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: [
+              ['@babel/preset-env', {
+                targets: {
+                  node: '18' // Match your Netlify runtime or even 16
+                },
+                useBuiltIns: 'usage',
+                corejs: 3,
+              }]
+            ],
+            plugins: ['@babel/plugin-proposal-optional-chaining', '@babel/plugin-proposal-nullish-coalescing-operator'],
           },
         },
       },
